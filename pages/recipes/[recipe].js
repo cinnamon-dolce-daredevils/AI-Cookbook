511e7853952ebf6beec1ecd2e6b4992263e6bbb4
codydibella <codibella@gmail.com>
2023-03-16 11:17:36 -0500

    added our ai model into the app

---
+++ pages/recipes/[recipe].js
@@ -0,0 +1,64 @@
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import fetch from "node-fetch";
+import styles from "../../styles/index.module.css";
+import { useRouter } from 'next/router';
+
+export default function Recipe() {
+  const [result, setResult] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const { recipe } = router.query;
+
+  async function onLoad() {
+    setIsLoading(true);
+    try {
+      const response = await fetch("../api/recipe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          recipe: recipe,
+        }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (recipe) {
+      onLoad();
+    }
+  }, [recipe]);
+
+  return (
+    <div className={styles.body}>
+      <Head>
+        <title>{recipe}</title>
+        <link rel="icon" href="/images/forkman.jpg" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/images/forkman.jpg" className={styles.icon} />
+        <h3>{isLoading ? 'Loading...' : recipe}</h3>
+        {isLoading ? (
+          <img src="/images/fridge.gif" />
+        ) : (
+          <h4 className={styles.recipe}>{result}</h4>
+        )}
+      </main>
+    </div>
+  );
+}
