941acef3e08b5e2c6234c3cd52fdb238a503c0a1
ryanpearl48 <ryanpearl98@gmail.com>
2023-03-16 15:04:13 -0700

    Auth first sadness

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { Provider } from 'next-auth/client';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default MyApp;
