e2e0a77d7ee8cfc33558785568c634dadf156d09
codydibella <codibella@gmail.com>
2023-03-16 19:58:26 -0500

    second call to spoonacular working correctly.

--- pages/ingredients/index.js
+++ pages/ingredients/index.js
@@ -6,23 +6,43 @@ import styles from "../../styles/index.module.css";
 const apiKey = "2513e401e1424711aabbf36076908290";
 
 async function callAutocompleteApi(input) {
-  const response = await fetch(
-    `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${apiKey}&query=${encodeURIComponent(
-      input
-    )}&number=10`
-  );
-
-  if (response.status !== 200) {
-    throw new Error(`Request failed with status ${response.status}`);
+    const response = await fetch(
+      `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${apiKey}&query=${encodeURIComponent(
+        input
+      )}&number=10&metaInformation=true`
+    );
+  
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+  
+    const data = await response.json();
+    return data;
   }
+  
+  
 
-  const data = await response.json();
-  return data;
-}
+  async function fetchIngredientDetails(id) {
+    const response = await fetch(
+      `https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${apiKey}&amount=100&unit=grams`
+    );
+  
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+  
+    const data = await response.json();
+    return data;
+  }
+  
+  
+  
+  
 
 export default function AddIng() {
   const [ingredientsInput, setIngredientsInput] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedIngredient, setSelectedIngredient] = useState(null);
 
   async function handleInputChange(event) {
     const input = event.target.value;
@@ -41,10 +61,20 @@ export default function AddIng() {
     }
   }
 
-  function handleSuggestionClick(suggestion) {
-    setIngredientsInput(suggestion.name);
+  async function handleSuggestionClick(suggestion) {
+    setIngredientsInput("");
     setSuggestions([]);
+  
+    try {
+      const ingredientDetails = await fetchIngredientDetails(suggestion.id);
+      setSelectedIngredient(ingredientDetails);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
   }
+  
+  
 
   return (
     <div className={styles.body}>
@@ -52,7 +82,7 @@ export default function AddIng() {
         <title>Pantry Popper</title>
         <link rel="icon" href="/images/forkman-removebg.png" />
       </Head>
-
+  
       <main className={styles.main}>
         <img src="/images/forkman-removebg.png" className={styles.icon} />
         <h3>Whatchu got in yo pantry?</h3>
@@ -72,6 +102,36 @@ export default function AddIng() {
             </li>
           ))}
         </ul>
+        {selectedIngredient && (
+  <>
+    <p>{selectedIngredient.name}</p>
+    <img src={`https://spoonacular.com/cdn/ingredients_100x100/${selectedIngredient.image}`} alt={selectedIngredient.name} />
+    {selectedIngredient.nutrition && selectedIngredient.nutrition.nutrients && (
+      <>
+        <p>
+          Calories:{" "}
+          {selectedIngredient.nutrition.nutrients.find((n) => n.name === "Calories")?.amount || "N/A"} kcal
+        </p>
+        <p>
+          Carbs:{" "}
+          {selectedIngredient.nutrition.nutrients.find((n) => n.name === "Carbohydrates")?.amount || "N/A"} g
+        </p>
+        <p>
+          Fat:{" "}
+          {selectedIngredient.nutrition.nutrients.find((n) => n.name === "Fat")?.amount || "N/A"} g
+        </p>
+        <p>
+          Protein:{" "}
+          {selectedIngredient.nutrition.nutrients.find((n) => n.name === "Protein")?.amount || "N/A"} g
+        </p>
+      </>
+    )}
+  </>
+)}
+
+
+
+
       </main>
       <Link href={"/"}>Return to Home</Link>
     </div>
