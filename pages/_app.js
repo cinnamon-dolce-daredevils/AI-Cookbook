a849b40034ac30af80183861a3e06bbce1b7400e
ryanpearl48 <ryanpearl98@gmail.com>
2023-03-29 14:14:28 -0700

    added localstorage to light and dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,62 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import React, { useState } from 'react'
-import { SWRConfig } from "swr";
-// below are roboto fonts from google
+// _app.js
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import React, { useState, useEffect } from 'react';
+import { SWRConfig } from 'swr';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Layout from '@/components/Layout';
-//imports theme
-import { ThemeProvider} from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-// should only be used for TS. awaiting import { createEmotionCache } from '../src/createEmotionCache';
-//the below line replaces the above line
+import { lightMode, darkMode } from '../styles/themes';
 
-import { lightMode, darkMode } from '../styles/themes'
-
-// line below allows us to use the isLightMode and setIsLightMode
-// in other files
 export const Context = React.createContext();
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
-  let themeSetter = darkMode;
-  let [mode, setMode] = useState('darkMode');
-  let [isLightMode, setIsLightMode] = useState(false);
   const [supabase] = useState(() => createBrowserSupabaseClient());
-  if (isLightMode == true) {
-	  themeSetter = lightMode;
-	  
-  } else {
-	  themeSetter = darkMode;
+  const [isLightMode, setIsLightMode] = useState(null);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedTheme = localStorage.getItem('theme');
+      setIsLightMode(storedTheme === 'lightMode');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLightMode !== null) {
+      localStorage.setItem('theme', isLightMode ? 'lightMode' : 'darkMode');
+    }
+  }, [isLightMode]);
+
+  if (isLightMode === null) {
+    return null;
   }
+
+  const theme = isLightMode ? lightMode : darkMode;
+
   return (
-		<SessionContextProvider
-		supabaseClient={supabase}
-		initialSession={pageProps.initialSession}
-		>
-		<Context.Provider value={[isLightMode, setIsLightMode]}>
-			<ThemeProvider theme={themeSetter}>
-					<SWRConfig
-						value={{
-							refreshInterval: 5000,
-						}}
-					>
-						<Layout>
-							<CssBaseline />
-							<Component {...pageProps} />
-						</Layout>
-					</SWRConfig>
-			</ThemeProvider>
-		</Context.Provider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Context.Provider value={[isLightMode, setIsLightMode]}>
+        <ThemeProvider theme={theme}>
+          <SWRConfig
+            value={{
+              refreshInterval: 5000,
+            }}
+          >
+            <Layout>
+              <CssBaseline />
+              <Component {...pageProps} />
+            </Layout>
+          </SWRConfig>
+        </ThemeProvider>
+      </Context.Provider>
     </SessionContextProvider>
-	);
+  );
 }
 export default App;
