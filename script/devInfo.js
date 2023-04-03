bb5c0e2b0cdf9a046008e39b03c2866ac6090dc8
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-04-03 15:40:38 -0400

    Update devInfo.js

--- script/devInfo.js
+++ script/devInfo.js
@@ -1,28 +1,31 @@
 export const DevInfo = [
 	{
 		name: "Ryan Pearl",
-		description: "Ryan is an Air Force Veteran, avid hockey fan and gym enthusiast. Coding has always been his dream, and the Military benefits have made that a reality!",
+		description:
+			"Ryan is an Air Force Veteran, avid hockey fan and gym enthusiast. Coding has always been his dream, and the Military benefits have made that a reality!",
 		avatar: "/images/ryanpic1.png",
 		linkedIn: "https://www.linkedin.com/in/ryanpearl48/",
 		GitHub: "https://github.com/ryanpearl48",
 	},
 	{
 		name: "Cody DiBella",
-		description: "hi, I am a good dev",
+		description:
+			"Cody is a tech enthusiast who loves diving into new challenges and expanding his knowledge. When he's not busy exploring the world of coding, you can find him playing video games and spending time with his wife, Hannah, and their two adorable dogs, Teach and Duncan.",
 		avatar: "/images/codypic.png",
 		linkedIn: "https://www.linkedin.com/in/codydibella/",
 		GitHub: "https://github.com/CodyDiBella",
 	},
 	{
 		name: "Ben Villafranca",
-		description: "Ben is a Marine veteran and college graduate of Rutgers - New Brunswick. If he isn't at his computer coding, he's probably running with his dog or cooking with his fiance.",
+		description:
+			"Ben is a Marine veteran and college graduate of Rutgers - New Brunswick. If he isn't at his computer coding, he's probably running with his dog or cooking with his fiance.",
 		avatar: "/images/benpic.png",
 		linkedIn: "https://www.linkedin.com/in/ben-villafranca/",
 		GitHub: "https://github.com/bevilla203",
 	},
 	{
 		name: "Jay Yuan",
-		description: "hi, I am a good dev",
+		description:"Jay is a tech-enthusiastic individual with a background in media and business strategies. When he's not coding, he is staying active with weight or with volleyball.",
 		avatar: "/images/jaypic.png",
 		linkedIn: "https://www.linkedin.com/in/jayqyuan/",
 		GitHub: "https://github.com/jayqyuan",
